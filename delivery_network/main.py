54f18b41e8a93a11071f616e2de126696e965176
eleroux46 <eloise.leroux@ensae.fr>
2023-03-15 22:42:54 +0100

    correction min_power 	modified:   delivery_network/graph.py 	modified:   delivery_network/main.py

---
+++ delivery_network/main.py
@@ -0,0 +1,10 @@
+from graph import Graph, graph_from_file, UnionFind, compare, estimate_time
+
+
+data_path = "input/"
+file_name = "network.01.in"
+
+g = graph_from_file(data_path + file_name)
+#print(g)
+compare(2)
+
