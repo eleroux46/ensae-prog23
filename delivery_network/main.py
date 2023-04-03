a6b4cfd874dd23ab47a0fd5244994fc3de52f694
eleroux46 <eloise.leroux@ensae.fr>
2023-04-03 19:42:20 +0200

    output routes 	modified:   delivery_network/main.py 	modified:   tests/test_s1q6_minimal_power.py 	modified:   delivery_network/__pycache__/graph.cpython-310.pyc 	modified:   delivery_network/graph.py 	modified:   output/routes.1.out 	modified:   output/routes.2.out 	modified:   output/routes.5.out 	input/trucks.0.in 	input/trucks.1.in 	input/trucks.2.in 	output/routes.10.out 	output/routes.1bis.out 	output/routes.3.out 	output/routes.4.out 	output/routes.6.out 	output/routes.7.out 	output/routes.8.out 	output/routes.9.out 	tests/test_stock_results.py

--- delivery_network/main.py
+++ delivery_network/main.py
@@ -1,4 +1,4 @@
-from graph import Graph, graph_from_file, UnionFind, compare, estimate_time, stock_results
+from graph import Graph, graph_from_file, UnionFind, compare, estimate_time, stock_results, Catalogue, catalogue_from_file, backpack_algorithm
 
 
 data_path = "input/"
@@ -9,14 +9,17 @@ h=g.kruskal()
 print(h)
 h.graph[1]
 #compare(2)
-#estimate_time(2)
-stock_results(2)
+#estimate_time(5)
+#stock_results(10)
 #print(g.build_caracteristics())
 #print(h.list_rank, h.list_parent)
 #print(g.connected_components())
-print(g.min_power_kruskal(10,18))
+#print(g.min_power_kruskal(6,11))
 # print(g.get_path_with_kruskal(15, 18))
 #print(h.min_power(10, 10))
 #print(val=h.graph.values())
 #print(val[0])
+
+#print(catalogue_from_file(1))
+#print(backpack_algorithm(1,1))
 
